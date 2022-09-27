eacd7638c18eb647173c8c10f75da4d7781e069f
suthiri131 <113505822+suthiri131@users.noreply.github.com>
2022-09-28 01:16:05 +0800

    Update serviceCheck.groovy

--- serviceCheck.groovy
+++ serviceCheck.groovy
@@ -1,10 +1,10 @@
 def timer(){
     if (!params.TIMER.isEmpty()) 
     { 
-        environment {
-            timeout_mins = "${params.TIMER}"
-                 } 
-            timeout(time: env.timeout_mins.toInteger(), unit: 'MINUTES')       
+//         environment {
+//             timeout_mins = "${params.TIMER}"
+//                  } 
+//             timeout(time: env.timeout_mins.toInteger(), unit: 'MINUTES')       
             echo 'success'
     }    
 }
