f3572f324a0aa490ea95adf7d2931783865d64fc
suthiri131 <113505822+suthiri131@users.noreply.github.com>
2022-10-03 09:07:29 +0800

    Update serviceCheck.groovy

--- serviceCheck.groovy
+++ serviceCheck.groovy
@@ -1,12 +1,32 @@
-def timer(){
-    if (!params.TIMER.isEmpty()) 
-    { 
-        def TIME_OUT = params.TIMER.toInteger()
-        options {
-            timeout(time:TIME_OUT, unit: 'MINUTES')    
-        }
-
-    }    
+def split () {
+  String list = "${params.ENDPOINT}";
+            String[] parts = list.split(","); 
 }
+
+// def timer(){
+//     if (!params.TIMER.isEmpty()) 
+//     { 
+//         environment {
+//             timeout_mins = "${params.TIMER}"
+//                  } 
+//             timeout(time: env.timeout_mins.toInteger(), unit: 'MINUTES')     
+//         {
+//             echo 'success'
+//         }
+//     }    
+// }
+
+def endpoint1 (web) {
+  final String url = web
+  int status = sh(script: "curl -sLI -w '%{http_code}' $url -o /dev/null", returnStdout: true)
+  if (status != 200 && status != 201) {
+    error("Returned status code = $status when calling $url")
+}
+  else {
+    echo "OK"
+  }
+  
+}
+
 
 return this
