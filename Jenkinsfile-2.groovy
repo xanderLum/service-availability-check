2af910132a84cd5be94f614826c691b80d8c69a6
suthiri131 <113505822+suthiri131@users.noreply.github.com>
2022-10-12 02:34:13 +0800

    Update Jenkinsfile-2.groovy

--- Jenkinsfile-2.groovy
+++ Jenkinsfile-2.groovy
@@ -7,11 +7,10 @@ def splitEndpoint () {
 def invokeEndpoint (endpoint) {
   String result
   while (currentBuild.duration<TIMER.toInteger()*1000) {
+   Date date = new Date(); 
+   echo "Executed at - $date"
   int time=TIME_INTERVAL.toInteger()
   for (String values:endpoint) {
-    echo values
-    Date date = new Date(); 
-   echo "Executed at - $date"
       int status = sh(script: "curl -sLI -w '%{http_code}' $values -o /dev/null", returnStdout: true)
   if (status == 200 ) {
     result="OK"
